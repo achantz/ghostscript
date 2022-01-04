f3fd6103644d281dfe8594c76492eeacd7feb73c
Angelo Chantziantoniou <achantz@gmail.com>
2022-01-04 11:23:47 -0700

    init

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompressionService } from './compression/compression.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CompressionService],
 })
-export class AppModule {}
+export class AppModule {
+  public async compressPDF(path: string) {
+    return;
+  }
+}
